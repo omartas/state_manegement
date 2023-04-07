1a1f052a439f368c9343c5df8bd037c9ce9d44cb
omartas <omerftas0@gmail.com>
2023-04-08 02:14:34 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/satremo/state_manegement/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.satremo.state_manegement
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  String sehir ="Adana";
+  void changeCity(String newCity){
+    sehir=newCity;
+    setState(() {
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('callback kullanımı'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(child: SolWidgetA(sehir: sehir,)),
+            Expanded(child: SagWidgetA(sehir: sehir,callback: changeCity,))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SolWidgetA extends StatelessWidget {
+  final String sehir;
+
+  const SolWidgetA({Key? key,required this.sehir}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.yellow,
+        child: Column(
+          children: [
+            Text(
+              'Sol Widget',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              sehir,
+              style: TextStyle(fontSize: 20),
+            )
+          ],
+        ));
+  }
+}
+
+class SagWidgetA extends StatelessWidget {
+  final Function callback;
+  final String sehir;
+
+  const SagWidgetA({Key? key,required this.sehir,required this.callback}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: Column(children: [
+        Text(
+          'SagWidget A',
+          style: TextStyle(fontSize: 20),
+        ),
+        SagWidgetB(sehir: sehir,callback: callback,)
+      ]),
+    );
+  }
+}
+
+class SagWidgetB extends StatelessWidget {
+  final String sehir;
+  final Function callback;
+  const SagWidgetB({Key? key,required this.sehir,required this.callback,}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: 180,
+      color: Colors.purple,
+      child: Column(children: [
+        Text(
+          'SagWidget B',
+          style: TextStyle(fontSize: 20),
+        ),
+        SagWidgetC(sehir: sehir,callback: callback)
+      ]),
+    );
+  }
+}
+
+class SagWidgetC extends StatelessWidget {
+  final String sehir;
+  final Function callback;
+
+  const SagWidgetC({Key? key,required this.sehir,required this.callback}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.white,
+        height: 150,
+        width: 160,
+        child: Column(children: [
+          Text(
+            'SagWidget C',
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            sehir,
+            style: TextStyle(fontSize: 20),
+          ),
+          TextField(onChanged:(input) {callback(input);})
+        ]),
+      ),
+    );
+  }
+}
